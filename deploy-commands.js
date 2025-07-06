01bcd05e9d22e4d387894834c13d5d7eb0f61b3b
Ethen Roth <Ethenfm@gmail.com>
2025-07-06 10:54:28 -0600

    initial commit

---
+++ deploy-commands.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+
+const commands = [
+    {
+        name: 'say',
+        description: 'says what you want it to say',
+        options: [
+            {
+                name: 'message',
+                type: 3, // STRING
+                description: 'The message to say',
+                required: true,
+            },
+        ],
+    }
+    ];
+
+const rest = new REST({ version: '10' }).setToken(process.env.discord_token);
+
+(async () => {
+    try {
+        console.log('registering slash commands...');
+
+        await rest.put(Routes.applicationGuildCommands(process.env.client_id, process.env.guild_id), {
+            body: commands,
+        });
+
+        console.log('successfully registered slash commands');
+    } catch (error) {
+        console.error(error);
+    }
+})();
---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ],
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand() || interaction.commandName !== 'say') return;
+        const message = interaction.options.getString('message');
+        // defer / send / delete 
+        await interaction.deferReply({ ephemeral: true });
+        await interaction.channel.send(message);
+        await interaction.deleteReply();
+
+});
+
+client.login(process.env.discord_token)
+    .then(() => console.log('Bot is online!'))
