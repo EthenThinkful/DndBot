e0a4c9c8c0334ae51c3c1c8d42ea57bdff1aef66
Ethen Roth <Ethenfm@gmail.com>
2025-07-06 15:48:38 -0600

    throwing error but working

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, MessageFlags } = require('discord.js');
 const {InteractionType,
 	InteractionResponseFlags,
 	InteractionResponseType} = require('discord-interactions');
@@ -15,11 +15,33 @@ client.once('ready', () => {
 });
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand() || interaction.commandName !== 'narrate') return;
-    const message = interaction.options.getString('message');
-    await interaction.deferReply({flags: InteractionResponseFlags.EPHEMERAL});
-    await interaction.channel.send(message);
-    await interaction.deleteReply();
+    try {
+        if (!interaction.isChatInputCommand() || interaction.commandName !== 'narrate') return;
+        const message = interaction.options.getString('message');
+        await interaction.reply({content: interaction.options.getString('message'), flags: MessageFlags.Ephemeral});
+        console.log('se');
+        // await interaction.deferReply({flags: Flags.Ephemeral});
+        console.log("op");
+        // await interaction.channel.send(message);
+        console.log('willy willy');
+        // await interaction.deleteReply();
+        console.log(`crazy`);
+
+        // await interaction.defer({flags: InteractionResponseFlags.EPHEMERAL});
+        // console.log('Interaction deferred');
+        // await interaction.followUp({content: message });
+        // await interaction.deleteReply();
+        // console.log(`Deferred interaction for message: ${message}`);
+        // await interaction.reply({
+        //     content: message,
+        //     flags: InteractionResponseFlags.EPHEMERAL // Makes the response visible only to the user who invoked the command
+        // });
+        // await interaction.deferReply({flags: InteractionResponseFlags.EPHEMERAL});
+        // await interaction.channel.send(message);
+        // await interaction.deleteReply();
+    } catch (error) {
+        console.error('Error handling interaction:', error);
+    }
 });
 
 client.login(process.env.discord_token)
