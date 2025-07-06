5c63efdf7de913cd7478302ddaba037f2f1e8ca0
Ethen Roth <Ethenfm@gmail.com>
2025-07-06 12:57:17 -0600

    flash popup ack

--- index.js
+++ index.js
@@ -14,16 +14,13 @@ client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
-// client.on('interactionCreate', async interaction => {
-//     if (!interaction.isChatInputCommand() || interaction.commandName !== 'narrate') return;
-//     // console.log(`Received command: ${interaction}`);
-//         // const message = interaction.options.getString('message');
-//         // // defer / send / delete 
-//         // await interaction.deferReply({ flags: InteractionResponseFlags.EPHEMERAL });
-//         // await interaction.channel.send(message);
-//         // await interaction.deleteReply();
-
-// });
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand() || interaction.commandName !== 'narrate') return;
+    const message = interaction.options.getString('message');
+    await interaction.deferReply({flags: InteractionResponseFlags.EPHEMERAL});
+    await interaction.channel.send(message);
+    await interaction.deleteReply();
+});
 
 client.login(process.env.discord_token)
     .then(() => console.log('Bot is online!'))
