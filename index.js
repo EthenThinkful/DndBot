9e9c40f9ef4e2cf295988ceaddd368eba500019c
Ethen Roth <Ethenfm@gmail.com>
2025-07-06 12:22:35 -0600

    works without any message handling?

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
+const {InteractionType,
+	InteractionResponseFlags,
+	InteractionResponseType} = require('discord-interactions');
 
 const client = new Client({
     intents: [
@@ -11,15 +14,16 @@ client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand() || interaction.commandName !== 'narrate') return;
-        const message = interaction.options.getString('message');
-        // defer / send / delete 
-        await interaction.deferReply({ ephemeral: true });
-        await interaction.channel.send(message);
-        await interaction.deleteReply();
+// client.on('interactionCreate', async interaction => {
+//     if (!interaction.isChatInputCommand() || interaction.commandName !== 'narrate') return;
+//     // console.log(`Received command: ${interaction}`);
+//         // const message = interaction.options.getString('message');
+//         // // defer / send / delete 
+//         // await interaction.deferReply({ flags: InteractionResponseFlags.EPHEMERAL });
+//         // await interaction.channel.send(message);
+//         // await interaction.deleteReply();
 
-});
+// });
 
 client.login(process.env.discord_token)
     .then(() => console.log('Bot is online!'))
